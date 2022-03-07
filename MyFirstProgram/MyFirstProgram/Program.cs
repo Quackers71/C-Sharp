7f80fb969c2093a8f2a59b12204ec03a1bf8197f
ThePebble <robq71@gmail.com>
2022-03-07 20:21:29 +0000

    MyFirstProgram

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("& I Love Nando's");
+            Console.Beep();
+        }
+    }
+}
