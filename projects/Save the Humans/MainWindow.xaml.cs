c0b2f140ed41e069d87a9d61caf01ecf9cdeb54b
ThePebble <robq71@gmail.com>
2022-03-19 10:42:58 +0000

    copied MainWindow.xaml from GH

--- projects/Save the Humans/MainWindow.xaml.cs
+++ projects/Save the Humans/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Save_the_Humans
 {
@@ -36,13 +37,24 @@ namespace Save_the_Humans
             ContentControl enemy = new ContentControl();
             enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
-            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), 
+                random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
         {
-            throw new NotImplementedException();
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6))),
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(propertyToAnimate));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
