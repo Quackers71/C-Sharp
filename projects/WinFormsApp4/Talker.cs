cababb7b32a73f0ca0d721673b2eb7264bdd7320
thePebble <robq71@gmail.com>
2022-03-28 16:46:01 +0100

    initial commit for Talker Tester App

---
+++ projects/WinFormsApp4/Talker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int count = 0; count < numberOfTimes; count++)
+            {
+                finalString += thingToSay;
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+    }
+}
