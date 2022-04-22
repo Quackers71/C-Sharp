ad77cf707cace1c2dd40484cbb3117d1126bc196
thePebble <robq71@gmail.com>
2022-04-22 13:19:40 +0100

    Added Biggest Ears Method & Button

--- projects/ElephantSwap/Form1.Designer.cs
+++ projects/ElephantSwap/Form1.Designer.cs
@@ -32,14 +32,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(59, 24);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button1.Location = new System.Drawing.Point(110, 51);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 22);
+            this.button1.Size = new System.Drawing.Size(150, 47);
             this.button1.TabIndex = 0;
             this.button1.Text = "Lloyd";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,10 +48,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(59, 61);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button2.Location = new System.Drawing.Point(110, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(81, 22);
+            this.button2.Size = new System.Drawing.Size(150, 47);
             this.button2.TabIndex = 1;
             this.button2.Text = "Lucinda";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,10 +59,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(59, 98);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button3.Location = new System.Drawing.Point(110, 209);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 22);
+            this.button3.Size = new System.Drawing.Size(150, 47);
             this.button3.TabIndex = 2;
             this.button3.Text = "Swap!";
             this.button3.UseVisualStyleBackColor = true;
@@ -69,24 +70,36 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(59, 137);
+            this.button4.Location = new System.Drawing.Point(110, 292);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(81, 23);
+            this.button4.Size = new System.Drawing.Size(150, 49);
             this.button4.TabIndex = 3;
             this.button4.Text = "Hi!";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 381);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(178, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Biggest Ears";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(193, 183);
+            this.ClientSize = new System.Drawing.Size(370, 470);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -101,5 +114,6 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
     }
 }
--- projects/ElephantSwap/Form1.cs
+++ projects/ElephantSwap/Form1.cs
@@ -42,5 +42,27 @@ namespace ElephantSwap
             lloyd.EarSize = 4321;
             lloyd.WhoAmI();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Elephant[] elephants = new Elephant[7];
+            elephants[0] = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            elephants[1] = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            elephants[2] = new Elephant() { Name = "Larry", EarSize = 42 };
+            elephants[3] = new Elephant() { Name = "Lucille", EarSize = 32 };
+            elephants[4] = new Elephant() { Name = "Lars", EarSize = 44 };
+            elephants[5] = new Elephant() { Name = "Linda", EarSize = 37 };
+            elephants[6] = new Elephant() { Name = "Humphrey", EarSize = 45 };
+
+            Elephant biggestEars = elephants[0];
+            for (int i = 1; i < elephants.Length; i++)
+            {
+                if (elephants[i].EarSize > biggestEars.EarSize)
+                {
+                    biggestEars = elephants[i];
+                }
+            }
+            MessageBox.Show(biggestEars.Name + "'s ear size is " + biggestEars.EarSize.ToString(), "Biggest Ears!");
+        }
     }
 }
